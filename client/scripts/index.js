bac19f209014228f8b22f8ad5159d19d2eb5fcd7
developeroka <developeroka@gmail.com>
2018-10-01 02:45:16 +0330

    Hidden overflow body on open sidenav

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -15,13 +15,15 @@ $('.hamburger').onclick = function toggleChange() {
     {
         $('.sidenav').style.width = "0";
         $('.main').style.marginLeft = "0";
+        $('.main').style.marginRight = "0";
         this.classList.remove('nav');
     }
     else
     {
-    this.classList.add('nav');
-    $('.sidenav').style.width = "300px";
-    $('.main').style.marginLeft = "300px";
+        this.classList.add('nav');
+        $('.sidenav').style.width = "300px";
+        $('.main').style.marginRight = "-300px";
+        $('.main').style.marginLeft = "300px";
     }
 }
 
