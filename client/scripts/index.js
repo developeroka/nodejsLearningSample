3053189a00df97b7c83be9bf3db022c4a4267f3f
developeroka <developeroka@gmail.com>
2018-10-02 21:48:31 +0330

    right-side-nav script has been written

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,5 +1,6 @@
 
 const $ = query => document.querySelector(query);
+const $$ = query => document.querySelectorAll(query);
 
 localStorage.setItem('productsId','[]')
 const addBtn = $('.addToCart');
@@ -18,6 +19,11 @@ $('.hamburger').onclick = function toggleChange() {
 
         $('.sidenav').classList.add('side-deactive');
         $('.main').classList.add('main-leftsidenav-second-transition');
+
+        
+        $('.sidenav').classList.remove('side-deactive');
+        $('.main').classList.remove('main-leftsidenav-second-transition');
+
        
         this.classList.remove('nav');
     }
@@ -32,6 +38,31 @@ $('.hamburger').onclick = function toggleChange() {
         $('.main').classList.add('main-leftsidenav-first-transition');
     }
 }
+
+
+$('.cart').onclick = function toggleChange() {
+    if(this.classList.contains('right-side-nav'))
+    {
+        $('.right-sidenav').classList.remove('rightside-nav-active');
+        $('.main').classList.remove('main-rightsidenav-first-transition');
+        $('.right-sidenav').classList.add('rightside-nav-deactive');
+        $('.main').classList.add('main-rightsidenav-second-transition');
+    
+        $('.right-sidenav').classList.remove('rightside-nav-deactive');
+        $('.main').classList.remove('main-rightsidenav-second-transition');
+
+        this.classList.remove('right-side-nav');
+    }
+    else
+    {
+        this.classList.add('right-side-nav');
+        $('.right-sidenav').classList.remove('rightside-nav-deactive');
+        $('.main').classList.remove('main-rightidenav-second-transition');
+        $('.right-sidenav').classList.add('rightside-nav-active');    
+        $('.main').classList.add('main-rightsidenav-first-transition');
+    }
+}
+
 
 
 
